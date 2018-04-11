102903cfa614b4d3e2f9b2aed614b1944ec9e495
Petr Koutný <petr.koutny@drivecontrol.cz>
2018-04-11 20:07:10 +0200

    druha aktivita a numericka klavesnice

--- app/src/main/java/com/example/petrkoutny/switcherv10/CardActivity.kt
+++ app/src/main/java/com/example/petrkoutny/switcherv10/CardActivity.kt
@@ -2,7 +2,12 @@ package com.example.petrkoutny.switcherv10
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_card.*
 
 class CardActivity : AppCompatActivity() {
 
@@ -12,8 +17,18 @@ class CardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card)
+
+        et_address.setOnEditorActionListener(object: TextView.OnEditorActionListener{
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                if (actionId==EditorInfo.IME_ACTION_DONE){
+                    val address = et_address.text
+                    return true
+                }
+                return false
+            }
+        })
     }
 
 
-    fun btn1_OnClick(view: View){}
+
 }
