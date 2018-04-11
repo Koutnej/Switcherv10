146c64d90d941e7cc93d81090ea467a3c20c93b3
Petr Koutný <petr.koutny@drivecontrol.cz>
2018-04-11 19:48:16 +0200

    druha aktivita a klavesnice

---
+++ app/src/main/java/com/example/petrkoutny/switcherv10/CardActivity.kt
@@ -0,0 +1,19 @@
+package com.example.petrkoutny.switcherv10
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class CardActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card)
+    }
+
+
+    fun btn1_OnClick(view: View){}
+}
--- app/src/main/java/com/example/petrkoutny/switcherv10/MainActivity.kt
+++ app/src/main/java/com/example/petrkoutny/switcherv10/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.petrkoutny.switcherv10
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+    fun btn1_OnClick(view:View){
+        val intent:Intent=Intent(this, CardActivity::class.java)
+        startActivity(intent)
     }
 }
