5b06bc7904074b030dc667757884a48a687cf646
Petr Koutný <petr.koutny@drivecontrol.cz>
2018-04-11 20:07:42 +0200

    druha aktivita a numericka klavesnice

--- app/src/main/java/com/example/petrkoutny/switcherv10/CardActivity.kt
+++ app/src/main/java/com/example/petrkoutny/switcherv10/CardActivity.kt
@@ -11,9 +11,6 @@ import kotlinx.android.synthetic.main.activity_card.*
 
 class CardActivity : AppCompatActivity() {
 
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card)
